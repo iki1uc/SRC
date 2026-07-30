93fca84223959c7850631e5f1015ad4a2d09889f
iki1uc <entertrainer@web.de>
2026-07-30 18:39:51 +0200

    Add PQ_STATION definition in station.js

---
+++ RESPO/SDSA/co/PQ/station.js
@@ -0,0 +1,8 @@
+import { Station } from "../station.js";
+
+export const PQ_STATION = Station("PQ", {
+    form: "Prioritäts‑Form",
+    struktur: "Prioritäts‑Struktur",
+    verhalten: "Prioritäts‑Verhalten",
+    präsenz: "Prioritäts‑Präsenz"
+});
