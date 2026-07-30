9c9910ecf7a93df6149da2e3fa57beb4289bf845
iki1uc <entertrainer@web.de>
2026-07-30 18:16:19 +0200

    Create station.js

---
+++ RESPO/station.js
@@ -0,0 +1,11 @@
+import { Station } from "../station.js";
+import o from "./o.md";
+import a from "./a.md";
+import r from "./r.md";
+
+export const RESPO_STATION = Station("RESPO", {
+    form: o,
+    struktur: a,
+    verhalten: r,
+    präsenz: "RESPO‑Anker"
+});
