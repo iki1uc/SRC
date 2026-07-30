b75a3ad003035b257f5be02255deda39f3c8f0ca
iki1uc <entertrainer@web.de>
2026-07-30 18:40:21 +0200

    Create station.js

---
+++ RESPO/SDSA/co/PP/station.js
@@ -0,0 +1,8 @@
+import { Station } from "../station.js";
+
+export const PP_STATION = Station("PP", {
+    form: "Prozess‑Form",
+    struktur: "Prozess‑Struktur",
+    verhalten: "Prozess‑Verhalten",
+    präsenz: "Prozess‑Präsenz"
+});
