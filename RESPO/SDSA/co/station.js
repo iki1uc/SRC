f16519a3bce2f520ccab66146e1105dce7acc39d
iki1uc <entertrainer@web.de>
2026-07-30 18:39:11 +0200

    Create station.js

---
+++ RESPO/SDSA/co/station.js
@@ -0,0 +1,8 @@
+import { Station } from "../station.js";
+
+export const CO_STATION = Station("CO", {
+    form: "Operator‑Form",
+    struktur: "Operator‑Struktur",
+    verhalten: "Operator‑Verhalten",
+    präsenz: "Operator‑Präsenz"
+});
