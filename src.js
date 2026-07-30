c6c8d8524acd13f3e3c5e9fe96a3ff8635253286
iki1uc <entertrainer@web.de>
2026-07-30 18:10:25 +0200

    Update src.js

--- src.js
+++ src.js
@@ -10,6 +10,16 @@ export const SRC = {
             degree: 360,
             fulfillment: 100,
             manifest: "körperisiert"
+     import { SRC } from "./src.js";
+
+import form from "./Koerperisierung/form/f.md";
+import struktur from "./Koerperisierung/Struktur/s.md";
+import verhalten from "./Koerperisierung/Verhalten/v.md";
+import präsenz from "./Koerperisierung/Präsenz/p.md";
+
+const körper = SRC.körperisieren(form, struktur, verhalten, präsenz);
+
+        
         };
     }
 };
