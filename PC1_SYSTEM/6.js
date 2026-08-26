c8332747be27ec30d62fa944b1956ec8f4d567f4
iki1uc <entertrainer@web.de>
2026-08-26 05:59:16 +0200

    Update 6.js

--- PC1_SYSTEM/6.js
+++ PC1_SYSTEM/6.js
@@ -1,16 +1,39 @@
-export function RUN3(y, x, z, q, f, d) {
+export function RUN3() {
 
-    const triList = [TRI.y, TRI.x, TRI.z, TRI.q, TRI.f, TRI.d];
+    const triList = Object.values(TRI);
 
     triList.forEach(tri => {
-        WIRBEL_ABFUHR(tri);
-        TRI_BOOMERANG(tri);
+
+        switch (tri.module) {
+
+            case "TRI_BOOMERANG":   // 3
+                TRI_BOOMERANG(tri);
+                break;
+
+            case "WIRBEL_ABFUHR":   // 4
+                WIRBEL_ABFUHR(tri);
+                break;
+
+            case "ECHO_HALL":       // 1 & 2
+                ECHO_HALL_CHECK(tri);
+                break;
+
+            case "TMP_BALANCE":     // 5
+                TMP_BALANCE(tri);
+                break;
+
+            case "TMP_CONTROL":     // 6
+                TMP_CONTROL(tri);
+                break;
+        }
     });
 
     AXIOM_LEARN(triList);
 
     return triList.map(tri => ({
+        id: tri.id,
         name: tri.name,
+        role: tri.role,
         echo: tri.tmp * 0.3,
         hall: tri.re * 0.2,
         missing: ECHO_HALL_CHECK(tri).missing
