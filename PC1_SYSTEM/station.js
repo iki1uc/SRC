dbe94232f573ad2e9277085ebcb40f459a7b381d
iki1uc <entertrainer@web.de>
2026-08-26 03:22:35 +0200

    Update station.js

--- PC1_SYSTEM/station.js
+++ PC1_SYSTEM/station.js
@@ -1,1 +1,11 @@
+import { TMP_Kraft } from "./src.js";
+
+export const MARKET = {
+    production: TMP_Kraft("Production"),
+    service:    TMP_Kraft("Service"),
+    data:       TMP_Kraft("Data"),
+    energy:     TMP_Kraft("Energy"),
+    logistics:  TMP_Kraft("Logistics"),
+    craft:      TMP_Kraft("Craft")
+};
 
