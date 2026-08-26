05921760d9091e6da5b0b1ffd65b564a4ebff63f
iki1uc <entertrainer@web.de>
2026-08-26 04:15:04 +0200

    Update main.js

--- STADT_UI/main.js
+++ STADT_UI/main.js
@@ -2,12 +2,24 @@ import { MARKET } from "../PC1_SYSTEM/station.js";
 import { RESPO } from "../PC1_SYSTEM/RESPO.js";
 
 export function updateUI() {
-    Object.keys(MARKET).forEach(m => {
-        const r = RESPO.react(MARKET[m]);
 
-        document.getElementById(`${m}_role`).innerText = r.role;
-        document.getElementById(`${m}_eff`).innerText = r.effizienz.toFixed(2);
-        document.getElementById(`${m}_well`).innerText = r.wohlbefinden.toFixed(2);
-        document.getElementById(`${m}_bind`).innerText = r.bindung.toFixed(2);
-    });
+    for (const name in MARKET) {
+
+        const result = RESPO.react(MARKET[name]);
+
+        setText(`${name}_role`, result.role);
+        setText(`${name}_ra`, result.ra);
+        setText(`${name}_re`, result.re);
+        setText(`${name}_kraft`, result.kraft);
+        setText(`${name}_eff`, result.effizienz.toFixed(2));
+        setText(`${name}_well`, result.wohlbefinden.toFixed(2));
+        setText(`${name}_bind`, result.bindung.toFixed(2));
+        setText(`${name}_freude`, result.freude.toFixed(2));
+        setText(`${name}_treue`, result.treue.toFixed(2));
+    }
+}
+
+function setText(id, value) {
+    const el = document.getElementById(id);
+    if (el) el.innerText = value;
 }
