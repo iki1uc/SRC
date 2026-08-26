fde7f50fc7543a09fb0c0835bb2b010ef2fe73fe
iki1uc <entertrainer@web.de>
2026-08-26 03:22:16 +0200

    Update ROOT.js

--- PC1_SYSTEM/ROOT.js
+++ PC1_SYSTEM/ROOT.js
@@ -1,1 +1,13 @@
+import { MARKET } from "./station.js";
+import { RESPO } from "./RESPO.js";
+
+export function SYSTEM_START() {
+    console.log("PC1_SYSTEM aktiviert.");
+
+    // Initiale Reaktion aller Märkte
+    Object.keys(MARKET).forEach(m => {
+        const r = RESPO.react(MARKET[m]);
+        console.log(m, r);
+    });
+}
 
