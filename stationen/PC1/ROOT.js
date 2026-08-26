8ebff18bc101851ee1c304fda742697bea273f06
iki1uc <entertrainer@web.de>
2026-08-26 04:13:44 +0200

    Create ROOT.js

---
+++ stationen/PC1/ROOT.js
@@ -0,0 +1,11 @@
+import { MARKET } from "./station.js";
+import { RESPO } from "./RESPO.js";
+
+export function SYSTEM_START() {
+    console.log("PC1_SYSTEM aktiviert.");
+
+    Object.keys(MARKET).forEach(m => {
+        const r = RESPO.react(MARKET[m]);
+        console.log(m, r);
+    });
+}
