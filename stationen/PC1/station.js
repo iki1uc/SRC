424655e225eee1f448f58aa6ab4f648eba459824
iki1uc <entertrainer@web.de>
2026-08-26 04:22:21 +0200

    Update station.js

--- stationen/PC1/station.js
+++ stationen/PC1/station.js
@@ -2,12 +2,12 @@ import { TMP_Kraft } from "./src.js";
 
 export const STATION = {
 
-    // ⭐ 4 gleichwertige Rechner
+    //  4 gleichwertige Rechner
     PC_A: TMP_Kraft("PC_A"),
     PC_B: TMP_Kraft("PC_B"),
     PC_C: TMP_Kraft("PC_C"),
     PC_D: TMP_Kraft("PC_D"),
 
-    // ⭐ EIN Root (Norm-System)
+    //  EIN Root (Norm-System)
     ROOT: TMP_Kraft("ROOT")
 };
