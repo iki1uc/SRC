2bc57e71eb2b7bf25326f0de903493aba0fab2ad
iki1uc <entertrainer@web.de>
2026-08-26 05:27:59 +0200

    Update ROOT.js

--- stationen/PC1/ROOT.js
+++ stationen/PC1/ROOT.js
@@ -1,11 +1,18 @@
 import { MARKET } from "./station.js";
 import { RESPO } from "./RESPO.js";
+import { pipelineblitzCluster } from "./pipelineblitzCluster.js";
 
 export function SYSTEM_START() {
-    console.log("PC1_SYSTEM aktiviert.");
+    console.log("PC1_SYSTEM + PC Cluster aktiviert.");
 
+    // ⭐ 1. Markt-System einmalig starten
     Object.keys(MARKET).forEach(m => {
         const r = RESPO.react(MARKET[m]);
-        console.log(m, r);
+        console.log("MARKET:", m, r);
     });
+
+    // ⭐ 2. Cluster-Blitz dauerhaft starten
+    setInterval(() => {
+        pipelineblitzCluster();
+    }, 1000); // 1x pro Sekunde
 }
