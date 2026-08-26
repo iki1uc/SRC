c54fd9f236e102b36a9f939118de5640b75991de
iki1uc <entertrainer@web.de>
2026-08-26 05:52:31 +0200

    Update 3.js

--- PC1_SYSTEM/3.js
+++ PC1_SYSTEM/3.js
@@ -1,8 +1,8 @@
 export function TRI_BOOMERANG(tri) {
-    const overflow = tri.tmp - 1.2; // max TMP
+    const overflow = tri.tmp - 1.2;
     if (overflow > 0) {
-        tri.tmp -= overflow * 0.7;     // 70% zurückwerfen
-        return overflow * 0.3;         // 30% Restimpuls
+        tri.tmp -= overflow * 0.7;
+        return overflow * 0.3;
     }
     return 0;
 }
