146e645d6d698b0a6814a42d9139a3bde5fe887f
iki1uc <entertrainer@web.de>
2026-08-26 03:25:12 +0200

    Update main.js

--- STADT_UI/main.js
+++ STADT_UI/main.js
@@ -1,1 +1,14 @@
+import { MARKET } from "../PC1_SYSTEM/station.js";
+import { RESPO } from "../PC1_SYSTEM/RESPO.js";
+
+export function updateUI() {
+    Object.keys(MARKET).forEach(m => {
+        const r = RESPO.react(MARKET[m]);
+
+        document.getElementById(`${m}_role`).innerText = r.role;
+        document.getElementById(`${m}_eff`).innerText = r.effizienz.toFixed(2);
+        document.getElementById(`${m}_well`).innerText = r.wohlbefinden.toFixed(2);
+        document.getElementById(`${m}_bind`).innerText = r.bindung.toFixed(2);
+    });
+}
 
