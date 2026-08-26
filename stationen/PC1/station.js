670d58878fac4558547810746ad187d2295dd1f0
iki1uc <entertrainer@web.de>
2026-08-26 04:22:12 +0200

    Update station.js

--- stationen/PC1/station.js
+++ stationen/PC1/station.js
@@ -1,10 +1,13 @@
 import { TMP_Kraft } from "./src.js";
 
-export const MARKET = {
-    production: TMP_Kraft("Production"),
-    service:    TMP_Kraft("Service"),
-    data:       TMP_Kraft("Data"),
-    energy:     TMP_Kraft("Energy"),
-    logistics:  TMP_Kraft("Logistics"),
-    craft:      TMP_Kraft("Craft")
+export const STATION = {
+
+    // ⭐ 4 gleichwertige Rechner
+    PC_A: TMP_Kraft("PC_A"),
+    PC_B: TMP_Kraft("PC_B"),
+    PC_C: TMP_Kraft("PC_C"),
+    PC_D: TMP_Kraft("PC_D"),
+
+    // ⭐ EIN Root (Norm-System)
+    ROOT: TMP_Kraft("ROOT")
 };
