42428cbe0f3539ef88cb2e899a8ee32f8208cb26
iki1uc <entertrainer@web.de>
2026-08-26 05:50:13 +0200

    Update 2.js

--- PC1_SYSTEM/2.js
+++ PC1_SYSTEM/2.js
@@ -1,8 +1,8 @@
-export const PIXEL_SPACE = {
-    minX: 0,
-    maxX: 1920,
-    minY: 0,
-    maxY: 1080,
-    minZ: -200,
-    maxZ: 200
+export const TRI = {
+    1: { id: 1, code: "y", name: "Tri-Y", role: "Echo",      module: "ECHO_HALL",      x: 200, y: 300, z: 0, tmp: 1, ra: 1, re: 1 },
+    2: { id: 2, code: "x", name: "Tri-X", role: "Hall",      module: "ECHO_HALL",      x: 400, y: 300, z: 0, tmp: 1, ra: 1, re: 1 },
+    3: { id: 3, code: "z", name: "Tri-Z", role: "Boomerang", module: "TRI_BOOMERANG",  x: 600, y: 300, z: 0, tmp: 1, ra: 1, re: 1 },
+    4: { id: 4, code: "q", name: "Tri-Q", role: "Wirbel",    module: "WIRBEL_ABFUHR",  x: 300, y: 500, z: 0, tmp: 1, ra: 1, re: 1 },
+    5: { id: 5, code: "f", name: "Tri-F", role: "Balance",   module: "TMP_BALANCE",    x: 500, y: 500, z: 0, tmp: 1, ra: 1, re: 1 },
+    6: { id: 6, code: "d", name: "Tri-D", role: "Control",   module: "TMP_CONTROL",    x: 700, y: 500, z: 0, tmp: 1, ra: 1, re: 1 }
 };
