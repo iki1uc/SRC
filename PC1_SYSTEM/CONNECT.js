a67d5b5d2b907d1a29675a13943c4bd3e1fa7257
iki1uc <entertrainer@web.de>
2026-08-26 03:23:36 +0200

    Update CONNECT.js

--- PC1_SYSTEM/CONNECT.js
+++ PC1_SYSTEM/CONNECT.js
@@ -1,1 +1,12 @@
+export function SIX(p) {
+    const aktion = p.tmp;
+    const reaktion = p.ra;
+    const konsequenz = aktion * reaktion;
+
+    const zeit = 1;
+    const effizienz = konsequenz / zeit;
+    const wohlbefinden = effizienz / (p.kraft || 1);
+
+    return { aktion, reaktion, konsequenz, zeit, effizienz, wohlbefinden };
+}
 
