98ee1e81f358b93e30795306acab93df61d436ff
iki1uc <entertrainer@web.de>
2026-08-26 05:44:28 +0200

    Create 1.js

---
+++ PC1_SYSTEM/1.js
@@ -0,0 +1,8 @@
+export const TRI = {
+    y: { name: "Tri-Y", x: 200,  y: 300,  z: 0, role: "Echo",     tmp: 1, ra: 1, re: 1 },
+    x: { name: "Tri-X", x: 400,  y: 300,  z: 0, role: "Hall",     tmp: 1, ra: 1, re: 1 },
+    z: { name: "Tri-Z", x: 600,  y: 300,  z: 0, role: "Boomerang",tmp: 1, ra: 1, re: 1 },
+    q: { name: "Tri-Q", x: 300,  y: 500,  z: 0, role: "Wirbel",   tmp: 1, ra: 1, re: 1 },
+    f: { name: "Tri-F", x: 500,  y: 500,  z: 0, role: "Balance",  tmp: 1, ra: 1, re: 1 },
+    d: { name: "Tri-D", x: 700,  y: 500,  z: 0, role: "Control",  tmp: 1, ra: 1, re: 1 }
+};
